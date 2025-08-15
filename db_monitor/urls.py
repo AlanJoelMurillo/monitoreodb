775c7bcc348ab4d8970808c58fdaccd19f987a85
Server <server@juarez.gob.mx>
2025-08-15 11:48:20 -0600

    App funcionando en server

---
+++ db_monitor/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+
+from . import views
+
+urlpatterns = [
+    path("",views.show_status, name="status"),
+    path("reporte/<str:server>/",views.show_report,name="report"),
+    path("check_status",views.check_status,name="check_status")
+    
+]
---
+++ db_monitor/views.py
@@ -0,0 +1,132 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.decorators import login_required
+import pyodbc
+import environ
+import os
+from django.utils import timezone
+
+
+#variables de entorno
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env = environ.Env(DEBUG=(bool,False))
+env.read_env(os.path.join(BASE_DIR,".env"))
+
+#variables de entorno
+credentials = [
+        {"ip":env("DB_MPIO3_IP"),"pass":env("DB_MPIO3_PASS"), "name":env("DB_MPIO3_NAME")},
+        {"ip":env("DB_MPIO4_IP"),"pass":env("DB_MPIO4_PASS"), "name":env("DB_MPIO4_NAME")},
+        {"ip":env("DB_MPIOACT_IP"),"pass":env("DB_MPIOACT_PASS"), "name":env("DB_MPIOACT_NAME")},
+        {"ip":env("DB_MPIOAF_IP"),"pass":env("DB_MPIOAF_PASS"), "name":env("DB_MPIOAF_NAME")},
+        {"ip":env("DB_MPIOPRE_IP"),"pass":env("DB_MPIOPRE_PASS"), "name":env("DB_MPIOPRE_NAME")},
+        {"ip":env("DB_MPIOAE_IP"),"pass":env("DB_MPIOAE_PASS"), "name":env("DB_MPIOAE_NAME")},
+        {"ip":env("DB_MPIOPRU_IP"),"pass":env("DB_MPIOPRU_PASS"), "name":env("DB_MPIOPRU_NAME")},
+]
+
+
+#Mostrar estado de servidores
+@login_required
+def show_status(request):
+    newDic = {}         #diccionario con IPs y Status
+    i = 0               #variable de iteracion
+
+    #Se crea un string con las credenciales
+    for cred in credentials:
+        i = i + 1
+        conn_str = f'DRIVER={{FreeTDS}};SERVER={cred["ip"]};PORT=5000;DATABASE=master;UID=usr_monitorweb;PWD={cred["pass"]};TDS_Version=5.0;'
+
+        try: 
+            newDic[i] = {
+                 "id" : i,
+                 "ipaddr": cred["ip"],
+                 "status": "down",
+                 "server": cred["name"]
+            }
+            
+            conn = pyodbc.connect(conn_str,timeout=1)
+            cursor = conn.cursor()
+            cursor.execute("sp_who2")
+            newDic[i]["status"] = "running"
+            conn.close()
+
+            
+        except pyodbc.Error as e:
+            print(" Error de conexión o ejecución del SP:", e)
+
+        except Exception as e:
+            print(" Error inesperado:", e)
+            return HttpResponse("Error: " + str(e) )
+    now = timezone.now()
+
+    context = { 
+        "data": newDic,
+        "time" :now,
+        }
+    return render(request, "db_monitor/app.html",context)
+
+@login_required
+def check_status(request):
+    newDic = {}         #diccionario con IPs y Status
+    i = 0    
+    for cred in credentials:
+        i = i + 1
+        conn_str = f'DRIVER={{FreeTDS}};SERVER={cred["ip"]};PORT=5000;DATABASE=master;UID=usr_monitorweb;PWD={cred["pass"]};TDS_Version=5.0;'
+        try: 
+            newDic[i] = {
+                 "id" : i,
+                 "status": "down",
+            }
+            
+            conn = pyodbc.connect(conn_str,timeout=1)
+            cursor = conn.cursor()
+            cursor.execute("sp_who2")
+            newDic[i]["status"] = "running"
+            conn.close()
+
+        except pyodbc.Error as e:
+            print(" Error de conexión o ejecución del SP:", e)
+
+        except Exception as e:
+            print(" Error inesperado:", e)
+    now = timezone.now().isoformat()
+    context = { "data": newDic,
+                "time": now}
+    return JsonResponse(context)
+
+
+
+#Mostrar reporte de servidor
+
+@login_required
+def show_report(request,server):
+    for cred in credentials:
+         if cred["name"] == server:
+            conn_str = f'DRIVER={{FreeTDS}};SERVER={cred["ip"]};PORT=5000;DATABASE=master;UID=usr_monitorweb;PWD={cred["pass"]};TDS_Version=5.0;'
+            try: 
+                conn = pyodbc.connect(conn_str,timeout=1)
+                cursor = conn.cursor()
+                cursor.execute("sp_who2")
+                columnas = [col[0] for col in cursor.description]
+                resultados = [dict(zip(columnas, fila)) for fila in cursor.fetchall()]
+                context = {"reporte" : resultados}
+         
+                conn.close()
+                
+                return render(request,"db_monitor/reporte.html",context)
+            
+            except pyodbc.Error as e:
+                errorContext = {"error":e}
+                print(" Error de conexión o ejecución del SP:", e)
+                return render(request, "db_monitor/error.html",errorContext)
+
+            except Exception as e:
+                errorContext = {"error":e}
+                print(" Error inesperado:", e)
+                return render(request, "db_monitor/error.html",errorContext)
+
+def login(request):
+    return render(request,"db_monitor/login.html")
+    
+
+
+
